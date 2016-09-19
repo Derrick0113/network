f6e6d7a79340381ef1ecc455b244f690ecfaa51b
zhenglu.zl <zhenglu.zl@alipay.com>
2016-09-19 17:21:39 +0800

    finish project1/chat

--- projects/proj1_chat/client.py
+++ projects/proj1_chat/client.py
@@ -7,40 +7,59 @@ import utils
 import sys
 import select
 
+
 class client(object):
-    def __init__(self, name, host,port):
+    def __init__(self, name, host, port):
         self.name = name
         self.host = host
         self.port = int(port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.buffer = []
         try:
-            self.socket.connect((self.host,self.port))
-        except socket.error,e:
-            print utils.CLIENT_CANNOT_CONNECT.format(host,port)
+            self.socket.connect((self.host, self.port))
+        except socket.error, e:
+            print utils.CLIENT_CANNOT_CONNECT.format(host, port)
             sys.exit(-1)
-        self.socket.send(name) #注册名字
+        self.socket.send(name)  # 注册名字
+
+    def __del__(self):
+        self.socket.close()
 
     def run(self):
-        inputs = [self.socket]
-        outputs = [sys.stdin]
-        while(True):
+        inputs = [self.socket, sys.stdin]
+        outputs = []
+        sys.stdout.write(utils.CLIENT_MESSAGE_PREFIX)
+        sys.stdout.flush()
+        while (True):
             try:
-                readables, writeables, exceptional = select.select(inputs, outputs,[])
-            except select.error,e:
+                readables, writeables, exceptional = select.select(inputs, outputs, [])
+            except select.error, e:
                 print e
                 break
-            if self.socket in readables: #收到服务器信息
-                try:
-                    data = self.socket.recv(1024)
-                    print("收到服务器信息")
-                    print data
-                except socket.error, e:
-                    print e
-                    break
-            else: #客户端
-                print("请输入你要的指令")
-                raw_input("")
+            for rec in readables:
+                if self.socket == rec:
+                    try:
+                        data = self.socket.recv(1024)
+                        if (data == ""):
+                            self.socket.close()
+                            self.writeToStdout(utils.CLIENT_SERVER_DISCONNECTED.format(self.host, self.port))
+                            sys.exit(-1)
+                        self.writeToStdout(data)
+                    except socket.error, e:
+                        print e
+                        break
+                elif sys.stdin == rec:
+                    in_text = sys.stdin.readline()
+                    self.socket.send(in_text.ljust(200))
+                    sys.stdout.write(utils.CLIENT_MESSAGE_PREFIX)
+                    sys.stdout.flush()
 
+
+    def writeToStdout(self, data):
+        sys.stdout.write(utils.CLIENT_WIPE_ME)
+        sys.stdout.write('\r' + data + '\n')
+        sys.stdout.write(utils.CLIENT_MESSAGE_PREFIX)
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
@@ -48,5 +67,7 @@ if __name__ == '__main__':
         print "Usage: python client.py Name ServerHost ServerPort"
     else:
         argv = sys.argv[1:]
-        client(argv[0],argv[1],argv[2])
-
+        try:
+            client(argv[0], argv[1], argv[2]).run()
+        except KeyboardInterrupt, e:
+            del client
--- projects/proj1_chat/server.py
+++ projects/proj1_chat/server.py
@@ -7,35 +7,138 @@ import utils
 import sys
 import select
 
+
 class Server(object):
-    def __init__(self, host,port):
+    def __init__(self, port,host="0.0.0.0"):
         self.host = host
         self.port = int(port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.outputs = []
+        self.channels = {}
+        self.socketMapToName = {}
+        self.buff = {}
         try:
-            self.socket.bind((self.host,self.port))
+            self.socket.bind((self.host, self.port))
             self.socket.listen(5)
-        except socket.error,e:
+        except socket.error, e:
             print e
+
+    def stop(self):
+        try:
+            self.socket.close()
+        except socket.error, e:
+            print e
+
+    def __del__(self):
+        try:
+            self.socket.close()
+        except socket.error, e:
+            print e
+
+    def _command_(self, text, csk):
+        text = text.split(' ')
+        if text[0] == '/join':
+            if (len(text) != 2):
+                csk.send(utils.SERVER_JOIN_REQUIRES_ARGUMENT)
+                return
+            channel = text[1]
+            if channel in self.channels:
+                if (csk in self.channels[channel]):  # 用户已在
+                    return
+                else:  # 用户不在
+                    self.addToNewChannel(channel, csk)
+                    return
+            else:  # 未存在该channel
+                csk.send(utils.SERVER_NO_CHANNEL_EXISTS.format(channel))
+                return
+        elif text[0] == '/list':
+            csk.send("\n".join(self.channels.keys()))
+            return
+        elif text[0] == '/create':
+            if (len(text) != 2):  # 指令用法错误
+                csk.send(utils.SERVER_CREATE_REQUIRES_ARGUMENT)
+                return
+            else:
+                channel = text[1]
+                if channel in self.channels:
+                    csk.send(utils.SERVER_CHANNEL_EXISTS.format(channel))  # 已经存在
+                    return
+                else:
+                    self.channels[channel] = []  # 新建channel,初始化
+                    self.addToNewChannel(channel, csk, True)
+                    return
+        else:
+            csk.send(utils.SERVER_INVALID_CONTROL_MESSAGE.format(text[0]))
+        return
+
+    def addToNewChannel(self, channel, csk, create=False):
+        name, old_channel = self.socketMapToName[csk]
+        if (old_channel != None):
+            self.channels[old_channel].remove(csk)
+            self._sendChannelExcept(old_channel, csk, utils.SERVER_CLIENT_LEFT_CHANNEL.format(name))
+        self.socketMapToName[csk] = (name, channel)
+        self.channels[channel].append(csk)
+        if not create:
+            self._sendChannelExcept(channel, csk, utils.SERVER_CLIENT_JOINED_CHANNEL.format(name))
+        return
+
+    def _sendChannelExcept(self, channel, csk, text):
+        for e in self.channels[channel]:
+            if e != csk:
+                e.send(text)
 
     def run(self):
         inputs = [self.socket]
         while True:
             try:
-                readable,writeable,exceptional = select.select(inputs,self.outputs,[])
-            except select.error,e:
+                readable, writeable, exceptional = select.select(inputs, self.outputs, [])
+            except select.error, e:
                 break
             if self.socket in readable:
-                client,addr = self.socket.accept()
+                client, addr = self.socket.accept()
                 self.outputs.append(client)
                 inputs.append(client)
             for csk in readable:
-                csk.recv(1024) # 读取1024个字节
+                if (csk == self.socket):
+                    continue
+                text = csk.recv(200)
+                if text == "":
+                    # disconnect
+                    if(csk not in self.socketMapToName):
+                        inputs.remove(csk)
+                        continue
+                    (name, channel) = self.socketMapToName[csk]
+                    self._sendChannelExcept(channel,csk, utils.SERVER_CLIENT_LEFT_CHANNEL.format(name))
+                    del self.socketMapToName[csk]
+                    self.channels[channel].remove(csk)
+                    inputs.remove(csk)
+                    continue
+                if csk not in self.buff:
+                    self.buff[csk] = text
+                else:
+                    self.buff[csk] = self.buff[csk] + text
+                if len(self.buff[csk]) >= 200:
+                    text = self.buff[csk][0:200]
+                    self.buff[csk] = self.buff[csk][200:]
+                else:
+                    continue
 
-
+                text = text.strip(' ').strip('\n')
+                if (csk not in self.socketMapToName):  # 未起名
+                    self.socketMapToName[csk] = (text, None)
+                elif text.startswith('/'):  # 命令
+                    self._command_(text, csk)
+                else:  # 普通文字
+                    (name, channel) = self.socketMapToName[csk]
+                    if (channel == None):
+                        csk.send(utils.SERVER_CLIENT_NOT_IN_CHANNEL)
+                    else:
+                        self._sendChannelExcept(channel, csk, '[{0}] '.format(name) + text)
 
 
 if __name__ == '__main__':
-    server = Server("0.0.0.0", 8888)
-    server.run()
+    server = Server(sys.argv[1])
+    try:
+        server.run()
+    except KeyboardInterrupt, e:
+        server.stop()
